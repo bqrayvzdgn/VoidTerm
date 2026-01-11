b40b980abde6657ca32ffd6edd7cec9d263602fe
Buğra <bqrayvzdgn@gmail.com>
2026-01-11 13:25:28 +0300

    fix: remove New Workspace from tab context menu

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -354,17 +354,6 @@ export const TabBar: React.FC<TabBarProps> = ({ onNewTab, onCreateTab, onCloseTa
                 <span>{workspace.name}</span>
               </button>
             ))}
-            <div className="context-menu-divider" />
-            <button
-              className="context-menu-item"
-              onClick={async () => {
-                const id = await addWorkspace()
-                handleMoveToWorkspace(id)
-              }}
-            >
-              <span className="context-menu-icon add">+</span>
-              <span>New Workspace</span>
-            </button>
           </div>
         </>
       )}
